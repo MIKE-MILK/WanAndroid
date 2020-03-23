12c9697946b182d38ceed872c79e01cd8f024b0d
Override0330 <854098072@qq.com>
2020-03-23 22:54:37 +0800

    [Update]初始化工程 1. 添加README描述 2. 初始化Android工程 3. 增加.gitignore项目
    Signed-off-by: Override0330 <854098072@qq.com>

---
+++ app/src/main/java/com/mredrock/wanandroid/App.java
@@ -0,0 +1,17 @@
+package com.mredrock.wanandroid;
+
+import android.app.Application;
+
+/**
+ * @author huanglong
+ * @date 2020/3/23
+ * @description
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //App初始化操作
+    }
+}
